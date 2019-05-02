85583f9a5a9395bcd367a998069cc3f24cbde8cf
FRED-LAPTOP\Frederico <frederico.94@hotmail.com>
2019-05-02 13:28:01 +0100

    Controlador, Modelo e Rotas de Aeronave

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('aeronaves','AeronaveController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
