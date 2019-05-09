0ab86acfd65251ef5313dcc061351c9f4451df91
FRED-LAPTOP\Frederico <frederico.94@hotmail.com>
2019-05-09 12:22:02 +0100

    Vista a Mostrar Login/Register

--- resources/views/list-aeronaves.blade.php
+++ resources/views/list-aeronaves.blade.php
@@ -1,4 +1,4 @@
-@extends('master')
+@extends('layouts.app')
 @section('content')
 
 @if (count($aeronaves))
@@ -17,27 +17,29 @@
         </tr>
     </thead>
     <tbody>
-        @foreach ($aeronaves as $aeronave)        
-        <tr>
-            <td>{{$aeronave->matricula}}</td>
-            <td>{{$aeronave->marca}}</td>
-            <td>{{$aeronave->modelo}}</td>
-            <td>{{$aeronave->num_lugares}}</td>
-            <td>{{$aeronave->conta_horas}}</td>
-            <td>{{$aeronave->preco_hora}}</td>
-            <td>
-                <a class="btn btn-xs btn-primary" href="/Aeronaves/{{$aeronave->matricula}}/edit">Edit</a>
+        @foreach ($aeronaves as $aeronave)
+            @if ($aeronave->deleted_at == null)
+                <tr>
+                    <td>{{$aeronave->matricula}}</td>
+                    <td>{{$aeronave->marca}}</td>
+                    <td>{{$aeronave->modelo}}</td>
+                    <td>{{$aeronave->num_lugares}}</td>
+                    <td>{{$aeronave->conta_horas}}</td>
+                    <td>{{$aeronave->preco_hora}}</td>
+                    <td>
+                        <a class="btn btn-xs btn-primary" href="/Aeronaves/{{$aeronave->matricula}}/edit">Edit</a>
 
-                <form action="{{ action('AeronaveController@destroy', $aeronave->matricula) }}" method="POST" role="form" class="inline">
-                    @csrf
-                    @method('delete')
+                        <form action="{{ action('AeronaveController@destroy', $aeronave->matricula) }}" method="POST" role="form" class="inline">
+                            @csrf
+                            @method('delete')
 
-                    <input type="hidden" name="user_id" value="{{$aeronave->matricula}}">
-                    <button type="submit" class="btn btn-xs btn-danger">Delete</button>
+                            <input type="hidden" name="user_id" value="{{$aeronave->matricula}}">
+                            <button type="submit" class="btn btn-xs btn-danger">Delete</button>
 
-                </form>
-            </td>
-        </tr>
+                        </form>
+                    </td>
+                </tr>
+            @endif
         @endforeach
 </table>
     @else
