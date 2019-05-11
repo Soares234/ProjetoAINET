f9473a1de2beafadb6f20b67d19d52d7243057a9
ADMIN <ricardosoares1999@hotmail.com>
2019-05-11 19:55:38 +0100

    Usei php artisan make:mail OrderShipped, criou uma Mailable class for future emailing

--- config/app.php
+++ config/app.php
@@ -169,15 +169,12 @@ return [
         /*
          * Application Service Providers...
          */
-        App\Providers\AppServiceProvider::class,
+	
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
-        App\Providers\TelescopeServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-
-    ],
-
+	App\Providers\RouteServiceProvider::class,   
+],
     /*
     |--------------------------------------------------------------------------
     | Class Aliases
