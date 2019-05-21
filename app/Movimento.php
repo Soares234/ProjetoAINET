fc51613a820cd1f6ab691a9f7425fdb3cbaa4fc1
ADMIN <ricardosoares1999@hotmail.com>
2019-05-21 13:27:31 +0100

    Mudanças

--- app/Movimento.php
+++ app/Movimento.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Movimento extends Model
 {
-    //
+    // Overrides primary key
+    protected $primaryKey = 'id';
+
+    // Disables auto increment primary key
+    public $incrementing = true;
+
+    // Enables auto timestamps
+    public $timestamps = true;
+
+    use SoftDeletes;
+    protected $dates = ['deleted_at'];
+//Lista de mass fillables
+    protected $fillable = [
+        'conta_horas_inicial', 'conta_horas_final', 'data','hora_descolagem', 'hora_aterragem',
+        'aeronave','num_diario','num_servico','piloto_id','num_licenca_piloto','validade_licenca_piloto'
+        ,'',''
+    ];
+
+
 }
