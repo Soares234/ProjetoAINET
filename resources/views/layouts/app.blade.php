94601458c7e49d5747c05ef41ce4c9f7d8dfc0ff
FRED-LAPTOP\Frederico <frederico.94@hotmail.com>
2019-05-22 17:37:39 +0100

    dropdown MeuPerfil

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,6 +52,8 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="/socios/{{Auth::user()->id}}">Meu Perfil</a>
+
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
